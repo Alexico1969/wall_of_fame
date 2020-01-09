84b0ec7dbedae1d1fd404cfedf187125810ab00c
Alexico1969 <34400264+Alexico1969@users.noreply.github.com>
2020-01-09 13:53:58 +0100

    initial setup

---
+++ main.js
@@ -0,0 +1,88 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAw6810iK0KtbMYZG0rDYRP5cHuJrTqvMk",
+    authDomain: "todo-database-7aa86.firebaseapp.com",
+    databaseURL: "https://todo-database-7aa86.firebaseio.com",
+    projectId: "todo-database-7aa86",
+    storageBucket: "todo-database-7aa86.appspot.com",
+    messagingSenderId: "605547808103",
+    appId: "1:605547808103:web:5cc31b84babb014466dbc2"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// reference todo's collection
+var todoRef = firebase.database().ref('todolists');
+
+// get data from firebase database
+todoRef.on('value', gotData, errData)
+
+// when there is data
+function gotData(data){
+    var todos = data.val();
+    var keys = Object.keys(todos)
+    console.log(keys);
+    var list = document.getElementById('todolist');
+    list.innerHTML = "";
+    for (var i = 0; i < keys.length; i++){
+        var k = keys[i];
+        var todo = todos[k].item;
+        var done = todos[k].done;
+        console.log(todo, ": " , done , " key : " , k);
+        var li = document.createElement('li');
+        li.innerHTML = '<button id="' + k + '" class="btn" onclick="update(this.id)">' + todo + '</button>';
+        if (done == 'true'){
+            li.classList.add('strike');
+            li.innerHTML = todo;
+        }
+        list.appendChild(li);
+    }
+}   
+
+//when there is an error
+function errData(err){
+    console.log('error');
+    console.log(err)
+}
+
+//Listen for form submit
+document.getElementById('todo_form').addEventListener('submit', submitform);
+
+
+//submit form
+function submitform(e){
+    e.preventDefault();
+    
+    //get values
+    var todo = getInputVal('newtodo');
+   
+    //save todo
+    saveTodo(todo);
+
+    //clear input field
+    document.getElementById('newtodo').value = "";
+}
+
+//function to get form values
+
+function getInputVal(id){
+    return document.getElementById(id).value;
+}
+
+//save the new todo
+
+function saveTodo(item){
+    var newTodoRef = todoRef.push();
+    newTodoRef.set({
+        item: item,
+        done: 'false'
+    })
+}
+
+//delete data
+
+function update(id){
+    console.log("deleting: ", id);
+    let userRef = firebase.database().ref('todolists/' + id);
+    userRef.update({'done':'true'});
+}
